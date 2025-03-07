20064af16170ab647d199a689e681f037cedf0d6
Sam Weiss <sweiss@cct.ie>
2025-03-07 16:00:24 +0000

    Exercise 1 and 2 solutions

--- js exercises.js
+++ js exercises.js
@@ -1,1 +1,18 @@
 console.log("Welcome to the exercise solutions!");
+// 1. Write a JavaScript program that displays "Hello, World!" in the browser console.
+console.log("Hello, world!");
+// 2. Create a JavaScript function that takes two numbers as input, performs addition,
+// subtraction, multiplication, and division operations on them, and displays the results
+// in the browser console.
+function arithmetic(num1, num2) {
+    let sum = num1+num2;
+    let difference = num1-num2;
+    let product = num1*num2;
+    let ratio = num1/num2;
+    console.log(sum, difference, product, ratio);
+}
+arithmetic(10, 2);
+// 3. Write a JavaScript program that prints the numbers 1 to 10 using a for loop and
+// another version using a while loop.
+// 4. Write a JavaScript program that checks if a given number is even or odd and displays
+// the result in the browser console.
